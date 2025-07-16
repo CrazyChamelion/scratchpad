792b2b3441964d786376c059b4d76b7371924e90
siggy707 <siggy707@gmail.com>
2025-07-15 21:30:28 -0600

    white space

--- arcade/smiley.py
+++ arcade/smiley.py
@@ -9,10 +9,13 @@ arcade.draw_arc_filled(325,675,250,250,arcade.color.BLACK,45,275)
 arcade.draw_arc_filled(690,655,250,250,arcade.color.BLACK,0,360)
 
 arcade.draw_circle_filled(500, 500, 300, arcade.color.YELLOW)
+
 arcade.draw_circle_filled(350, 625, 50, arcade.color.BLACK)
 arcade.draw_circle_filled(650, 625, 50, arcade.color.BLACK)
+
 arcade.draw_arc_filled(500,350,200,200,arcade.color.BLACK,0,360)
 arcade.draw_arc_filled(500,350,200,200,arcade.color.YELLOW,0,180)
+
 arcade.draw_triangle_filled(450, 450, 550, 450, 500, 550, arcade.color.BLACK)
 
 
