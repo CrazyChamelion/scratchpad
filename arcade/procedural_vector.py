803f33647da142403acb07b88507573a0ba14966
siggy707 <siggy707@gmail.com>
2026-06-07 11:25:52 -0600

    oo example format

--- arcade/procedural_vector.py
+++ arcade/procedural_vector.py
@@ -1,6 +1,7 @@
 import arcade
 import math
 import random
+
 # Constants for window size and title
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -12,30 +13,38 @@ class Vector:
         self.x = x
         self.y = y
 
+
 # pull info out of the class example (procedural)
 def add_vec(a, b):
     return Vector(a.x + b.x, a.y + b.y)
 
+
 def subtract_vec(a, b):
     return Vector(a.x - b.x, a.y - b.y)
 
+
 def scale_vec(v, scale):
-    return Vector(v.x*scale, v.y*scale)
+    return Vector(v.x * scale, v.y * scale)
+
 
 def length_vec(v):
-    return math.sqrt(v.x*v.x + v.y*v.y)
+    return math.sqrt(v.x * v.x + v.y * v.y)
+
 
 def normalize_vec(v):
     l = length_vec(v)
     return scale_vec(v, 1 / l)
 
+
 def random_vec():
-    return Vector(random.randint(1,SCREEN_WIDTH),random.randint(1,SCREEN_HEIGHT))
+    return Vector(random.randint(1, SCREEN_WIDTH), random.randint(1, SCREEN_HEIGHT))
+
 
 class Arrow:
     def __init__(self, p, dir):
         self.p = p
         self.dir = dir
+
 
 # procedural draw arrow
 def draw_arrow(a):
@@ -60,24 +69,21 @@ class VectorDrawer(arcade.Window):
         arcade.set_background_color(arcade.color.BLACK)
         self.player = random_vec()
         self.enemies = []
-        for a in range (10):
+        for a in range(10):
             e = random_vec()
-            between = subtract_vec(e,self.player)
-            while length_vec(between) < 100: 
+            between = subtract_vec(e, self.player)
+            while length_vec(between) < 100:
                 e = random_vec()
-                between = subtract_vec(e,self.player)
-            self.enemies.append (e)
-                
-                 
+                between = subtract_vec(e, self.player)
+            self.enemies.append(e)
+
         self.arrows = []
         for e in self.enemies:
-            dir = subtract_vec(self.player,e)
+            dir = subtract_vec(self.player, e)
             p = length_vec(dir)
-            dir = normalize_vec(dir) 
-            dir = scale_vec(dir,50)
-            self.arrows.append(Arrow(e,dir))
-
-
+            dir = normalize_vec(dir)
+            dir = scale_vec(dir, 50)
+            self.arrows.append(Arrow(e, dir))
 
     def setup(self):
         """Initializes game variables and objects."""
@@ -88,9 +94,10 @@ class VectorDrawer(arcade.Window):
         self.clear()
         for a in self.arrows:
             draw_arrow(a)
-        arcade.draw_circle_filled(self.player.x , self.player.y , 10 , arcade.color.BLUE)
+        arcade.draw_circle_filled(self.player.x, self.player.y, 10, arcade.color.BLUE)
         for i in self.enemies:
-            arcade.draw_circle_filled(i.x , i.y , 10 , arcade.color.RED)
+            arcade.draw_circle_filled(i.x, i.y, 10, arcade.color.RED)
+
     def on_update(self, delta_time):
         """Handles game logic and movement."""
         pass
